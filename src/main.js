cd981ef4986f51ae342416e1def8cd1eaa8d33ad
feiyi <1428574339@qq.com>
2019-07-27 17:16:32 +0800

    试用

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant);
+
+Vue.use(VueAxios,axios);
 
 Vue.config.productionTip = false
 
